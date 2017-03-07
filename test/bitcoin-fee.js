53c630058554f017e3e82785fba798058fabd5d2
RyanZim <opensrc@ryanzim.com>
2017-03-07 11:49:20 -0500

    Print fees to test output for debugging

--- test/bitcoin-fee.js
+++ test/bitcoin-fee.js
@@ -13,7 +13,7 @@ test('fetchFee() works', t => {
   t.plan(BTCfee.SERVICES.length)
   Promise.all(BTCfee.SERVICES.map(service => BTCfee.fetchFee(service)))
   .then(arr => {
-    arr.forEach((item, i) => t.is(typeof item, 'number', `result for ${BTCfee.SERVICES[i]} is a number`))
+    arr.forEach((item, i) => t.is(typeof item, 'number', `result for ${BTCfee.SERVICES[i]} is a number (${item})`))
     t.end()
   })
   .catch(t.end)
