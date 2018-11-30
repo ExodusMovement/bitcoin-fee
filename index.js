d61f943cd798eb5e526ad67f302f3d6448c1ccdf
Faris Issa <farisissa8@gmail.com>
2018-11-30 15:06:29 +0000

    Remove dynamic require (#5)

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-exports.SERVICES = [
-  '21.co',
-  'blockcypher',
-  'bitpay',
-  'btc.com'
-]
+const endpoints = {
+  '21.co': require('./services/21.co'),
+  'blockcypher': require('./services/blockcypher'),
+  'bitpay': require('./services/bitpay'),
+  'btc.com': require('./services/btc.com')
+}
 
-const endpoints = {}
-exports.SERVICES.forEach(service => {
-  endpoints[service] = require('./services/' + service)
-})
+exports.SERVICES = Object.keys(endpoints)
 
 exports.fetchFee = function (service) {
   return endpoints[service].fetchFee()
